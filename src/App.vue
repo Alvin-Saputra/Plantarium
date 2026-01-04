aa71bfe5994c397ba7b94f393ee5ca94f7e17ccb
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2026-01-04 16:59:31 +0700

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+
+<template>
+  <v-app>
+    <header>
+      
+    </header>
+    <RouterView/>
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
